c91af57e6e9fa398e97c6c60c9fdc357c4341043
Kuba Podkomórka <jpodkomorka19@gmail.com>
2024-01-01 14:49:51 +0100

    Created first simple analysing logic

---
+++ car_scraping/analizer.py
@@ -0,0 +1,55 @@
+"""
+file that contains all the logic needed to analise the data in the car repository to get the cars with the best price
+(cheaper than the average price of the same car in the repository)
+"""
+
+import car_scraping.utils as utils
+from repository import car_repository as car_repo
+
+
+def analise_cars_with_best_price(cars_with_best_price: list):
+    """
+    Method that analises the cars with the best price and prints the results
+    :param cars_with_best_price: List of cars with the best price
+    """
+    if not cars_with_best_price:
+        print('No cars with best price found')
+        return
+
+    print(f'Found {len(cars_with_best_price)} cars with best price:')
+    for car in cars_with_best_price:
+        print(f'    {car.full_name} - {car.price_pln} PLN')
+        print(f'        {car.link}')
+
+    print(f'\nTop 5 cars with best price with best mileage:')
+    cars_with_best_price.sort(key=lambda x: x.mileage)
+    for car in cars_with_best_price[:5]:
+        print(f'    {car.full_name} - {car.mileage} km, {car.price_pln} PLN')
+        print(f'        {car.link}')
+
+    print(f'\nTop 5 cars with best price with best year:')
+    cars_with_best_price.sort(key=lambda x: x.year)
+    for car in cars_with_best_price[:5]:
+        print(f'    {car.full_name} - {car.year}, {car.price_pln} PLN')
+        print(f'        {car.link}')
+
+
+
+
+
+def get_cars_with_best_price(brand: str, model: str) -> list:
+    """
+    Method that returns the brand and model of cars with the best price
+    in the repository
+    :return: list of cars with the best price
+    """
+    cars = car_repo.get_all_cars_by_brand_and_model(brand, model)
+    if not cars:
+        print(f'No cars found for brand {brand} and model {model}')
+        return []
+
+    average_price = utils.get_average_price(cars)
+    print(f'Average price for {brand} {model} is {average_price}')
+
+    cars_with_best_price = [car for car in cars if car.price_pln < average_price]
+    analise_cars_with_best_price(cars_with_best_price)
--- car_scraping/utils.py
+++ car_scraping/utils.py
@@ -51,3 +51,13 @@ def get_brand_from_full_name(full_name: str) -> str:
     """
     brand = unidecode(full_name.split(' ')[0].lower())
     return format_brand(brand)
+
+
+def get_average_price(cars):
+    """
+    Calculate average price of cars in given list of cars
+    :param cars: List of Car objects
+    :return: Average price of cars in given list
+    """
+    prices = [car.price_pln for car in cars]
+    return sum(prices) / len(prices) if prices else None
