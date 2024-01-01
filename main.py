192a45ce25954a44f9b86797f6201f2469971bbd
Kuba Podkomórka <jpodkomorka19@gmail.com>
2024-01-01 14:50:09 +0100

    Added analysing to main

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 
 def main():
     from car_scraping.scraper import scrape_n_pages
-    scrape_n_pages(6)
-    print()
+    from car_scraping.analizer import get_cars_with_best_price
+    # Analyze the repository
+    # scrape_n_pages(6)
+    get_cars_with_best_price('skoda', 'fabia')
 
 
 if __name__ == '__main__':
