28cc77224ee17db4d54cd5b036ac2394fd3d66ff
Kuba Podkomórka <jpodkomorka19@gmail.com>
2024-01-03 21:11:07 +0100

    Updated main

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 
 def main():
     from car_scraping.scraper import scrape_n_pages
-    from car_scraping.analizer import get_cars_with_best_price
+    from car_scraping.analizer import get_brands_with_best_price
+    # scrape_n_pages(750, None, 278)
     # Analyze the repository
-    # scrape_n_pages(6)
-    get_cars_with_best_price('skoda', 'fabia')
+    get_brands_with_best_price('skoda', 'fabia', 2010, 2020,
+                               0, 200000, 'benzyna', 'manualna')
 
 
 if __name__ == '__main__':
