313f291fc8added7699c8312105f39778c7cc636
Kuba Podkomórka <jpodkomorka19@gmail.com>
2024-01-11 17:57:15 +0100

    Changed scraper.py to scrape more data and added option to scrape olx page too instead otomoto only

--- car_scraping/scraper.py
+++ car_scraping/scraper.py
@@ -1,16 +1,17 @@
 import random
 import time
 import requests
+from enum import Enum
 from bs4 import BeautifulSoup
-from math import ceil
 from car_scraping.car import Car
-import car_scraping.utils as utils
 import repository.car_repository as car_repo
-import repository.total_cars_repository as total_cars_repo
 
-URL = 'https://www.otomoto.pl/osobowe'
-NEW = 'nowe'
-USED = 'uzywane'
+
+class Page(Enum):
+    otomoto = 'otomoto'
+    otomoto_url = 'https://www.otomoto.pl/osobowe'
+    olx = 'olx'
+    olx_url = 'https://www.olx.pl/motoryzacja/samochody/'
 
 USER_AGENTS = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
@@ -28,131 +29,274 @@ headers = {
     'User-Agent': USER_AGENTS[0]
 }
 
+labels_to_find_otomoto = [
+    'Marka pojazdu',
+    'Model pojazdu',
+    'Przebieg',
+    'Pojemność skokowa',
+    'Moc',
+    'Rok produkcji',
+    'Rodzaj paliwa',
+    'Skrzynia biegów',
+    'Typ nadwozia',
+    'Kolor',
+    'Rodzaj koloru',
+    'Bezwypadkowy',
+    'Stan'
+]
 
-def scrape_total_used_new(car_brand=None) -> tuple[int, int]:  # Get the total number of used and new cars
-    used_url = utils.get_page_url(URL, USED, car_brand)
-    try:
-        response = requests.get(used_url, headers).text
-        soup = BeautifulSoup(response, 'html.parser')
-        n_used = soup.find('a', {'data-testid': 'select-used'}).get_text(strip=True)
-        n_new = soup.find('a', {'data-testid': 'select-new'}).get_text(strip=True)
-        return utils.extract_numbers_from_string(n_used), utils.extract_numbers_from_string(n_new)
-    except Exception as e:
-        print(f'Encountered problem while getting number of used and new cars on page{used_url}\nerror message: {e}')
-        return 0, 0
+labels_to_find_olx = [
+    'Model',
+    'Przebieg',
+    'Poj. silnika',
+    'Moc silnika',
+    'Rok produkcji',
+    'Paliwo',
+    'Skrzynia biegów',
+    'Typ nadwozia',
+    'Kolor',
+    'Stan techniczny',
+]
 
 
-def scrape_n_pages(n_pages: int, car_brand=None, from_page=1, min_sleep=4., max_sleep=7., print_steps=True) -> None:
+def scrape_n_pages(from_page=Page.otomoto, n_pages=1, start_page=1,
+                   min_sleep=4., max_sleep=7., print_steps=True) -> None:
     """
-    Main scraping method. It will iterate through pages and scrape cars info
+    Main scraping method. It will iterate through pages and scrape cars info and update total number of cars in db
+    :param from_page: Page to scrape from
     :param n_pages: Number of pages to scrape
     :param car_brand: Name of the car brand to scrape. If none given all possible brands will be scrapped
-    :param from_page: Number of page from which scrapping will start
+    :param start_page: Number of page from which scrapping will start
     :param min_sleep: Minimum time delay after request
     :param max_sleep: Maximum time delay after request
-    :param print_steps: Should method print the steps it's taking
+    :param print_steps: If True, method will print the steps it's taking
+    :return: None
     """
-    n_used, n_new = scrape_total_used_new(car_brand)  # Get the total number of used and new cars
-
-    used_ratio = n_used/(n_used+n_new)  # Calculate the ratio of used cars
-    n_pages_used = ceil(used_ratio * n_pages)  # Get the number of pages for the used cars based on the ratio
-    n_pages_new = n_pages - n_pages_used  # Get the number of pages for new cars
-
-    brands = set()
     if print_steps:
-        print(f'Scraping used cars...')
-    for i in range(from_page, from_page + n_pages_used):  # Iterate through n used pages
+        print(f'Scraping cars...')
+
+    for i in range(start_page, start_page + n_pages):
         if print_steps:
-            print(f'  Scraping page {i}')
+            print(f'  Scraping page {i} of cars from {from_page.value}?page={i}')
 
-        # Rotate between different User-Agents to mimic different browsers or devices
-        headers['User-Agent'] = random.choice(USER_AGENTS)
+        links = scrape_links_from_page(from_page.value + f'?page={i}')
 
-        page_used = utils.get_page_url(URL, USED, car_brand, i)  # Get url for used cars on page i
-        brands.update(scrape_cars_from_page(page_used, USED))  # Scrape used cars with given url & add brands to update
+        for car_link in links:
+            # Rotate between different User-Agents to mimic different browsers or devices
+            headers['User-Agent'] = random.choice(USER_AGENTS)
 
-        if print_steps:
-            print(f'  Delay after page {i} of used cars...')
-        time.sleep(random.uniform(min_sleep, max_sleep))  # Wait some time delay to mimic human behavior
+            scrape_car_info(car_link)
 
-    if print_steps:
-        print(f'Scraping new cars...')
-    for i in range(from_page, from_page + n_pages_new):  # Iterate through n new pages
-        if print_steps:
-            print(f'  Scraping page {i}')
-
-        headers['User-Agent'] = random.choice(USER_AGENTS)  # Change user agents
-
-        page_new = utils.get_page_url(URL, NEW, car_brand, i)  # Get url for new cars
-        brands.update(scrape_cars_from_page(page_new, NEW))  # Scrape new cars and add brands
-
-        if print_steps:
-            print(f'  Delay after page {i} of new cars...')
-        time.sleep(random.uniform(min_sleep, max_sleep))  # Wait
-
-    if print_steps:
-        print('Updating total number of used and new cars for scraped brands...')
-    total_cars_repo.update_or_create_total_cars('*', n_used, n_new)  # Update total number of used and new cars in db
-    for brand in brands:
-        if print_steps:
-            print(f'  Updating for {brand}')
-        total_used, total_new = scrape_total_used_new(brand)
-        total_cars_repo.update_or_create_total_cars(brand, total_used, total_new)
+            if print_steps:
+                print(f'    Delay after car at link {car_link} from page number {i}...')
+            time.sleep(random.uniform(min_sleep, max_sleep))  # Wait some time delay to mimic human behavior
 
 
-def scrape_cars_from_page(page: str, status=None) -> set[str]:
+def scrape_links_from_page(page: str) -> list[str]:
     """
     Method sends request to the page to scrape, then creates beautiful soup object to work with
-    :param page: url of page to scrape
-    :param status: used or new
+    :param page: Url of page to scrape
+    :return: List of links to cars
+    """
+    try:
+        cookie = {'cookie_name': 'cookie_value'}
+        session = requests.Session()
+        session.cookies.update(cookie)
+
+        response = session.get(page, headers=headers).text
+        soup = BeautifulSoup(response, 'html.parser')
+        # Check if page is otomoto or olx
+        if str(Page.otomoto_url.value) in page:
+            return get_links_otomoto(soup)
+        elif str(Page.olx_url.value) in page:
+            return get_links_olx(soup)
+        return []
+    except Exception as e:
+        print(f'Encountered problem on the page {page}, error message: {e}')
+        return []
+
+
+def get_links_otomoto(soup) -> list[str]:
+    """
+    Method that returns links to cars with BeautifulSoup object from response
+    :param soup: BeautifulSoup object from response
+    :return: List of links to cars
+    """
+    links = []
+    try:
+        # Get the div element storing all car offers on the page
+        search_results = soup.find('div', class_='ooa-r53y0q ezh3mkl11')
+        # Get all car offers elements
+        car_articles = search_results.find_all('article', class_='ooa-yca59n e1oqyyyi0')
+        for car_article in car_articles:  # Iterate through car offers elements
+            # Get the particular html elements representing needed car properties
+            url_element = car_article.find('a', href=True)
+            links.append(url_element['href'] if url_element is not None else None)
+        return links
+    except Exception as e:
+        print(f'Encountered problem while extracting car links from page, error message: {e}')
+        return []
+
+
+def get_links_olx(soup) -> list[str]:
+    """
+    Method that returns links to cars with BeautifulSoup object from response
+    :param soup: BeautifulSoup object from response
+    :return: List of links to cars
+    """
+    links = []
+    try:
+        # Get the div element storing all car offers on the page
+        search_results = soup.find('div', class_='css-oukcj3')
+        # Get all car offers elements
+        car_articles = search_results.find_all('div', class_='css-1sw7q4x')
+        for car_article in car_articles:  # Iterate through car offers elements
+            # Get the particular html elements representing needed car properties
+            url_element = car_article.find('a', href=True)
+            if Page.otomoto_url.value in url_element['href']:  # Some auctions on olx are from otomoto
+                links.append(url_element['href'])
+            else:
+                links.append('https://www.olx.pl' + url_element['href'] if url_element is not None else None)
+        return links
+    except Exception as e:
+        print(f'Encountered problem while extracting urls of cars, error message: {e}')
+        return []
+
+
+def scrape_car_info(page) -> None:
+    """
+    Method sends request to the page to scrape, then creates beautiful soup object to work with
+    :param page: Url of page to scrape
+    :return: None
     """
     try:
         response = requests.get(page, headers).text
         soup = BeautifulSoup(response, 'html.parser')
-        return save_cars(soup, status)
+        # Check if page is otomoto or olx
+        if str(Page.otomoto_url.value) in page:
+            save_cars_otomoto(soup, page)
+        elif str(Page.olx.value) in page:
+            save_cars_olx(soup, page)
     except Exception as e:
         print(f'Encountered problem on the page {page}, error message: {e}')
-        return set()
 
 
-def save_cars(soup: BeautifulSoup, status=None) -> set[str]:
+def save_cars_otomoto(soup: BeautifulSoup, link: str) -> None:
     """
-    Actual cars scrapping and saving in database with BeautifulSoup object.
+    Method that saves cars to database with BeautifulSoup object from response for otomoto
+    :param link: Link to car offer
     :param soup: BeautifulSoup object from response
-    :param status: used or new
     """
-    # Get the html elements representing single car offers
-    search_results = soup.find('div', class_='ooa-r53y0q e1y257vc10')
-    car_articles = search_results.find_all('article', class_='ooa-yca59n e1ajxysh0')
-    brands = set()
     try:
-        for car_article in car_articles:  # Iterate through car offers elements
-            # Get the particular html elements representing needed car properties
-            url_element = car_article.find('a', href=True)
-            full_name_element = car_article.find('h1')
-            list_element = car_article.find('div', class_='ooa-d3dp2q e1ajxysh2')
-            mileage_element = list_element.find('dd', {'data-parameter': 'mileage'})
-            fuel_type_element = list_element.find('dd', {'data-parameter': 'fuel_type'})
-            gearbox_element = list_element.find('dd', {'data-parameter': 'gearbox'})
-            year_element = list_element.find('dd', {'data-parameter': 'year'})
-            price_element = car_article.find('h3', class_='e1ajxysh16 ooa-1n2paoq er34gjf0')
-            car_to_add = Car(
-                    link=url_element['href'] if url_element is not None else None,
-                    full_name=full_name_element.get_text(strip=True) if full_name_element else None,
-                    url_brand='',
-                    mileage=int(mileage_element.get_text(strip=True).replace(" ", "")
-                                .removesuffix('km')) if mileage_element else None,
-                    fuel_type=fuel_type_element.get_text(strip=True) if fuel_type_element else None,
-                    gearbox=gearbox_element.get_text(strip=True) if gearbox_element else None,
-                    year=int(year_element.get_text(strip=True)) if year_element else None,
-                    status=status,
-                    price_pln=int(price_element.get_text(strip=True).replace(" ", "")) if price_element else None
-                )
-            car_to_add.url_brand = utils.get_brand_from_full_name(car_to_add.full_name)
-            brands.add(car_to_add.url_brand)
-            car_repo.add_car_if_not_exists(car_to_add)
-    except Exception as e:
-        print(f'Encountered problem while extracting cars data, error message: {e}')
-        return set()
+        # Dictionary to store label-value pairs
+        car_details = {}
 
-    return brands
+        # Get price element
+        price_element = (soup.find('h3', class_='offer-price__number eqdspoq4 ooa-o7wv9s er34gjf0')
+                         .text.replace(' ', ''))
+
+        # Find all div elements with specific class
+        details = soup.find_all("div", class_="ooa-162vy3d e18eslyg3")
+        # Filter the details list to keep only the elements that have labels we want to keep
+        details = [detail for detail in details if detail.find("p", class_="e18eslyg4 ooa-12b2ph5").text.strip() in labels_to_find_otomoto]
+
+        # Loop through each div to extract label-value pairs
+        for detail in details:
+            label = detail.find('p', class_="e18eslyg4 ooa-12b2ph5").text.strip()
+            if label in [labels_to_find_otomoto[0], labels_to_find_otomoto[1], labels_to_find_otomoto[6],
+                         labels_to_find_otomoto[7], labels_to_find_otomoto[8], labels_to_find_otomoto[9],
+                         labels_to_find_otomoto[11], labels_to_find_otomoto[12]]:
+                value = detail.find('a', class_="e16lfxpc1 ooa-1ftbcn2").text.strip()
+            else:
+                value = detail.find('p', class_="e16lfxpc0 ooa-1pe3502 er34gjf0").text.strip()
+
+            # Check if the label matches the ones we're interested in
+            if label in labels_to_find_otomoto:
+                car_details[label] = value
+
+        # Create car object
+        car = Car(
+            link=link,
+            brand=car_details[labels_to_find_otomoto[0]] if labels_to_find_otomoto[0] in car_details else None,
+            model=car_details[labels_to_find_otomoto[1]] if labels_to_find_otomoto[1] in car_details else None,
+            mileage=int(car_details[labels_to_find_otomoto[2]].replace('km', '').replace(' ', '')) if labels_to_find_otomoto[2] in car_details else None,
+            engine_capacity=int(car_details[labels_to_find_otomoto[3]].replace('cm3', '').replace(' ', '')) if labels_to_find_otomoto[3] in car_details else None,
+            engine_power=int(car_details[labels_to_find_otomoto[4]].replace('KM', '').replace(' ', '')) if labels_to_find_otomoto[4] in car_details else None,
+            year=int(car_details[labels_to_find_otomoto[5]]) if labels_to_find_otomoto[5] in car_details else None,
+            fuel_type=car_details[labels_to_find_otomoto[6]] if labels_to_find_otomoto[6] in car_details else None,
+            gearbox=car_details[labels_to_find_otomoto[7]] if labels_to_find_otomoto[7] in car_details else None,
+            body_type=car_details[labels_to_find_otomoto[8]] if labels_to_find_otomoto[8] in car_details else None,
+            colour=car_details[labels_to_find_otomoto[9]] if labels_to_find_otomoto[9] in car_details else None,
+            type_of_color=car_details[labels_to_find_otomoto[10]] if labels_to_find_otomoto[10] in car_details else None,
+            accident_free=car_details[labels_to_find_otomoto[11]] if labels_to_find_otomoto[11] in car_details else None,
+            state=car_details[labels_to_find_otomoto[12]] if labels_to_find_otomoto[12] in car_details else None,
+            price_pln=int(price_element.replace(' ', '')) if price_element else None
+        )
+
+        car_repo.add_car_if_not_exists(car)  # Add car to database
+
+    except Exception as e:
+        print(f'Encountered problem while extracting car data from page {link}, error message: {e}')
+
+
+def save_cars_olx(soup: BeautifulSoup, link: str) -> None:
+    """
+    Method that saves cars to database with BeautifulSoup object from response for olx
+    :param soup: BeautifulSoup object from response
+    :param link: Link to car offer
+    :return: None
+    """
+    try:
+        # Dictionary to store label-value pairs
+        car_details = {}
+
+        # Find the first 'a' element that stores the value of brand name
+        brand_element = soup.find(
+            lambda tag: tag.name == 'a' and tag.get('href', '').startswith('/motoryzacja/samochody/') and tag.get(
+                'href').count('/') == 4)
+
+        # Get the price element
+        price_element = soup.find('h3', class_='css-93ez2t')
+
+        # Find all list items with class "css-1r0si1e"
+        list_items = soup.find_all('li', class_='css-1r0si1e')
+
+        # Loop through each list item to extract label-value pairs
+        for item in list_items:
+            p_element = item.find('p', class_='css-b5m1rv')
+
+            # Check if the p element exists and does not contain a span (it is different and not contain ':')
+            if p_element and not p_element.find('span'):
+                label = p_element.text.split(': ')[0]
+                value = p_element.text.split(': ')[1]
+                car_details[label] = value
+
+        # Create car object
+        car = Car(
+            link=link,
+            brand=brand_element.get_text(strip=True) if brand_element else None,
+            model=car_details[labels_to_find_olx[0]] if labels_to_find_olx[0] in car_details else None,
+            mileage=int(car_details[labels_to_find_olx[1]].replace('km', '').replace(' ', '')) if labels_to_find_olx[1] in car_details else None,
+            engine_capacity=int(car_details[labels_to_find_olx[2]].replace('cm³', '').replace(' ', '')) if labels_to_find_olx[2] in car_details else None,
+            engine_power=int(car_details[labels_to_find_olx[3]].replace('KM', '').replace(' ', '')) if labels_to_find_olx[3] in car_details else None,
+            year=int(car_details[labels_to_find_olx[4]]) if labels_to_find_olx[4] in car_details else None,
+            fuel_type=car_details[labels_to_find_olx[5]] if labels_to_find_olx[5] in car_details else None,
+            gearbox=car_details[labels_to_find_olx[6]] if labels_to_find_olx[6] in car_details else None,
+            body_type=car_details[labels_to_find_olx[7]] if labels_to_find_olx[7] in car_details else None,
+            colour=car_details[labels_to_find_olx[8]] if labels_to_find_olx[8] in car_details else None,
+            type_of_color=None,
+            accident_free=car_details[labels_to_find_olx[9]] if labels_to_find_olx[9] in car_details else None,
+            state=None,
+            price_pln=int(price_element.text.strip().replace(' ', '').replace('zł', '')) if price_element else None
+        )
+
+        # Set the state of the car based on mileage
+        if car.mileage < 500:
+            car.state = 'Nowy'
+        else:
+            car.state = 'Używany'
+
+        car_repo.add_car_if_not_exists(car)  # Add car to database
+
+    except Exception as e:
+        print(f'Encountered problem while extracting car data from page {link}, error message: {e}')
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 
 
 def main():
-    from car_scraping.scraper import scrape_n_pages
+    from car_scraping.scraper import scrape_n_pages, Page
     from car_scraping.analizer import get_brands_with_best_price
-    # scrape_n_pages(750, None, 278)
+
+    # Scrape the cars
+    scrape_n_pages(Page.olx_url, 25, 1, 3.0, 5., True)
+    scrape_n_pages(Page.otomoto_url, 1000, 1, 3.0, 5., True)
     # Analyze the repository
-    get_brands_with_best_price('skoda', ['octavia'], 2018, 2025,
-                               0, 250000, ['benzyna'], ['automat'], '')
+    # get_brands_with_best_price('bmw', [''], 2006, 2006,
+    #                            0, 300000, ['benzyna', 'petrol'], ['manual'], '')
 
 
 if __name__ == '__main__':
